768fffaa78dc9267531216586f03624ba1660cf4
dyc-1 <55264217+dyc-1@users.noreply.github.com>
2020-05-24 14:31:45 +0800

    Update settings.py

--- itstudio/itstudio/settings.py
+++ itstudio/itstudio/settings.py
@@ -80,10 +80,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'itstudio',
-        'USER': 'root',
-        'PASSWORD': '123456',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'USER': ,
+        'PASSWORD': ,
+        'HOST': ,
+        'PORT': ,
         'OPTIONS': {
             'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"',
             'charset': 'utf8mb4'
